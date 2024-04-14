1e17884b269e8dec7885ad44078edffeeffb8629
jamber751 <jaamber751@gmail.com>
2024-04-14 19:09:17 +0500

    fixup! Bucket sprite setup

--- src/bucket.py
+++ src/bucket.py
@@ -4,13 +4,12 @@ from pygame import sprite, image, mouse
 class Bucket(sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
-
-        self.image = image.load('graphics/bucket.png')
+        self.image = image.load('graphics/bucket.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=(400, 600))
 
-    def user_input(self):
+    def user_input(self) -> None:
         mouse_pos = mouse.get_pos()
         self.rect.centerx = mouse_pos[0]
 
-    def update(self):
+    def update(self) -> None:
         self.user_input()
